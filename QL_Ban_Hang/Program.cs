f1a7a775a064279c7d625b39f0e74861c1cc6e10
ThanhLong <ThanhLong@THANHLONG-PC>
2017-11-28 08:24:16 +0700

    Add project files.

---
+++ QL_Ban_Hang/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_Ban_Hang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PROJECT
+        }
+    }
+}
